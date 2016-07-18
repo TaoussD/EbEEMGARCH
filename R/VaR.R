d5bd9f275801efc2c100d8072f61fe50e7f4e5ee
Dimitri Taouss <drt2129@columbia.edu>
2016-07-18 14:25:52 +0200

    Add files via upload

---
+++ R/VaR.R
@@ -0,0 +1,52 @@
+InvSqrt <- function(Sigma, tol = sqrt(.Machine$double.eps)) {
+  n <- nrow(Sigma)
+  sp <- eigen(Sigma)
+  sp$vector %*% sqrt(diag(1 / pmax(abs(sp$values), tol))) %*% t(sp$vector)
+}
+
+
+residuals_DCC <-function(Omega,A,B,alpha,beta,S,eps,r=10,type)
+{
+  tol <- sqrt(.Machine$double.eps)
+  Qt <- array(dim = c(n, m, m))
+  Ht <- Qt
+  Qt[1,,] <- S
+  Qt.star <- diag(sqrt(diag(Qt[1,,])))
+  Qt.star.inv <- diag(sqrt(1 / pmax(diag(Qt[1,,]), tol)))
+  Rt <- Qt
+  Rt[1,,] <- Qt.star.inv %*% Qt[1,,] %*% Qt.star.inv
+
+
+  eta.star <- matrix(0, nrow = n, ncol = m)
+  ht <- matrix(0, nrow = n, ncol = m)
+  ht[1,] <- colMeans(eps[1:r,] ^ 2)
+  Ht[1,,] <- diag(sqrt(ht[1,])) %*% Rt[1,,] %*% diag(sqrt(ht[1,]))
+
+  for (t in 2:n) {
+    ht[t,] <- Omega + as.vector(A %*% (eps[t - 1,] ^ 2)) + B * ht[t - 1,]
+    eta.star[t,] <- eps[t,] / sqrt(ht[t,])
+    if (type == "Aielli") {
+      Qt[t,,] <- (1 - alpha - beta) * S + alpha * Qt.star %*% eta.star[t - 1,] %*% t(eta.star[t - 1,]) %*% Qt.star + beta * Qt[t - 1,,]
+    } else {
+      if (type == "Engle") {
+        Qt[t,,] <- (1 - alpha - beta) * S + alpha * eta.star[t - 1,] %*% t(eta.star[t - 1,]) + beta * Qt[t - 1,,]
+      } else {
+        print("Not a valid model")
+      }
+    }
+
+    Qt.star <- diag(sqrt(diag(Qt[t,,])))
+    Qt.star.inv <- diag(sqrt(1 / pmax(diag(Qt[t,,]), tol)))
+    Rt[t,,] <- Qt.star.inv %*% Qt[t,,] %*% Qt.star.inv
+    Ht[t,,] <- diag(sqrt(ht[t,])) %*% Rt[t,,] %*% diag(sqrt(ht[t,]))
+  }
+  eta <- matrix(0, nrow = n, ncol = 2)
+  for (t in 1:n) {
+    eta[t,] <- InvSqrt(as.matrix(var$Ht[t,,])) %*% Rend$sim[t,]
+  }
+
+
+  return(list(Ht=Ht,Rt=Rt,eta=eta))
+}
+
+residuals_DCC <- cmpfun(residuals_DCC)
